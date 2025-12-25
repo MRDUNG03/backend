e788cfac4a404564d2d8afb7b4910afbd3e4161e
Dung Vi <vythaidung2003@gmail.com>
2025-12-25 13:33:29 +0700

    Add env file and gitignore

--- model/connected_DB.py
+++ model/connected_DB.py
@@ -1,10 +1,15 @@
 import mysql.connector
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 
 def connected_DB():
     return mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="123456",
-        database="motordatabase",
+        host=os.getenv("MYSQL_HOST"),
+        user=os.getenv("MYSQL_USER"),
+        password=os.getenv("MYSQL_PASSWORD"),
+        database=os.getenv("MYSQL_DB"),
         auth_plugin='mysql_native_password'
     )
